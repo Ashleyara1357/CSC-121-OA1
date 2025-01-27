c54c0c64eeb9c081b9f873d625e47d5ea2cf8afd
Ashley Ramos <149848201+Ashleyara1357@users.noreply.github.com>
2025-01-26 19:36:32 -0500

    Add files via upload

---
+++ simple_messages.py
@@ -0,0 +1,8 @@
+#Assign a message to a variable and print it
+Phrase= "Message assigned to a variable."
+print(Phrase)
+
+#Change the value of the variable to a new message and print it
+Phrase = "This is a new message."
+print(Phrase)
+
