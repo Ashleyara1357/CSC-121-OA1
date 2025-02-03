d51f5ff163347fd388800ab0b88b72352fd8fb0b
Ashley Ramos <149848201+Ashleyara1357@users.noreply.github.com>
2025-02-02 20:15:30 -0500

    Add files via upload

---
+++ 3-3_My_Own_List.py
@@ -0,0 +1,8 @@
+# List of favorite modes of transportation
+transportation = ["Tesla Model S", "Harley-Davidson motorcycle", "Toyota Tacoma", "Nissan Rogue"]
+
+# Printing statements about each transportation mode
+print(f"I would like to own a {transportation[0]}.")
+print(f"Riding a {transportation[1]} would be an amazing experience.")
+print(f"A {transportation[2]} is great for off-road adventures.")
+print(f"The {transportation[3]} is a reliable and comfortable SUV.")
