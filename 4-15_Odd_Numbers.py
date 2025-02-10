d8a6b028fe90f6f77e6b293011afb611f9833676
Ashley Ramos <149848201+Ashleyara1357@users.noreply.github.com>
2025-02-09 22:06:02 -0500

    Add files via upload

---
+++ 4-15_Odd_Numbers.py
@@ -0,0 +1,8 @@
+# 4-6 Odd Numbers
+# This program prints all odd numbers from 1 to 20
+
+odd_numbers = list(range(1, 21, 2))  # Generates odd numbers from 1 to 20
+
+# Printing each odd number
+for number in odd_numbers:
+    print(number)
