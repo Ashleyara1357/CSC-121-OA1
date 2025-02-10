8ae10f7ddd889d458e7c6bb6322a03ff5ed3b640
Ashley Ramos <149848201+Ashleyara1357@users.noreply.github.com>
2025-02-09 19:40:40 -0500

    Add files via upload

---
+++ 4.2-Animals.py
@@ -0,0 +1,13 @@
+# List of animals with a common characteristic
+animals = ["eagle", "falcon", "hawk"]
+
+# Printing each animal name
+for animal in animals:
+    print(animal.title())
+
+# Printing a statement about each animal
+for animal in animals:
+    print(f"A {animal} is an incredible bird of prey.")
+
+# Common characteristic statement
+print("\nAll of these animals are powerful birds that soar through the sky!")
