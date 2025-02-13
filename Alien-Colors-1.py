848a4fc4ac5220f2dfa5c6a9b2a5f650ee7a88a2
Ashley Ramos <149848201+Ashleyara1357@users.noreply.github.com>
2025-02-12 19:25:48 -0500

    Add files via upload

---
+++ Alien-Colors-1.py
@@ -0,0 +1,15 @@
+# Author: Ashley Ramos
+# Assignment: 5.3 Alien Colors #3 (Final Revision - Green)
+# Class: CSC-121-OA1
+# Due: 2/16/2025
+
+# Assigning the alien's color
+alien_color = 'green'
+
+# If-Elif-Else chain to determine points
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points!")
+else:
+    print("You just earned 15 points!")
